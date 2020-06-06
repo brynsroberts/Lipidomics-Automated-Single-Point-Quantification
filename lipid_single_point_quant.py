4c08b32671fab2507029e45f73d15e8395dcc628
Bryan Roberts <bryan.roberts.cs@gmail.com>
2020-06-06 12:53:29 -0700

    initial commit

---
+++ lipid_single_point_quant.py
@@ -0,0 +1,161 @@
+
+"""lipid_single_point_quant.py: calculates concentrations for species based on internal stanadard heights"""
+
+__author__ = "Bryan Roberts"
+
+import pandas as pd
+import numpy as np
+import re
+import csv
+from pathlib import Path
+
+FIRST_COLUMN = 1
+FIRST_ROW = 1
+SECOND_ROW = 2
+
+
+def set_standards_from_csv(df):
+    """takes in csv file with standard information and generates dictionary of standards information
+    Parameters:
+        df (data frame): user excel sheet in pandas data frame
+    Returns:
+        dictionary of with all needed standards information filled in
+    """
+    file_path = input(
+        "Enter full file path for csv file containing standards information: ")
+    standards_file = open(file_path)
+    reader = csv.reader(standards_file)
+    standards_data = list(reader)
+
+    # populate standards dictionary from file
+    standards = {}
+    for row in range(FIRST_ROW, len(standards_data)):
+        name = standards_data[row][0]
+        concentration = standards_data[row][1]
+        standards[name] = {
+            "Row": 0, "ID": 0, "Concentration": concentration}
+
+    # populate Row and ID from data frame
+    set_standard_row_id(df, standards)
+    return standards
+
+
+def set_named_constant(df, pattern):
+    """sets column name from data frame based on regex pattern to named constant
+    Parameters:
+        df (data frame): user excel sheet in pandas data frame
+        pattern: regex pattern to match
+    Returns:
+        name of column matching regex pattern from data frame
+    """
+    regex_pattern = re.compile(pattern)
+    for column_name in df.columns:
+        found = regex_pattern.search(column_name.lower())
+        if found != None:
+            return column_name
+    return None
+
+
+def set_sample_name_list(df, pattern):
+    """returns all samples column headers in data frame to list
+    Parameters:
+        df (data frame): user excel sheet in pandas data frame
+        pattern: regex pattern to match
+    Returns:
+        list of sample names in data frame
+    """
+    sample_names = []
+    regex_pattern = re.compile(pattern)
+    for column_name in df.columns:
+        found = regex_pattern.search(column_name.lower())
+        if found != None:
+            sample_names.append(column_name)
+    return sample_names
+
+
+def set_standard_row_id(df, standards):
+    """sets row and id in standards dictionary from data frame
+    Parameters:
+        df (data frame): user excel sheet in pandas data frame
+        standards: dictionary of standards in formath {name: {"ID":, "Row", "Concentration"}}
+    Returns:
+        None - changes standards in place
+    """
+    for standard_name in standards:
+        for row in range(len(df)):
+            if standard_name == df[ANNOTATION_NAME_COLUMN][row]:
+                standards[standard_name]["Row"] = row
+                standards[standard_name]["ID"] = df[ISTD_MATCH_COLUMN][row]
+
+
+def find_matching_istd(df, row, standards):
+    """finds matching standard of feature based on matching iSTD ID
+    Parameters:
+        df (data frame): user excel sheet in pandas data frame
+        row: int, row currently being processed in data frame
+        standards: dictionary of standards in formath {name: {"ID":, "Row", "Concentration"}}
+    Returns:
+        standard_name: string of matching standard based on ID of current row
+    """
+    for standard_name in standards:
+        if standards[standard_name]['ID'] == df[ISTD_MATCH_COLUMN][row]:
+            return standard_name
+
+
+def calculate_results(df, sample_name_list, standards):
+    """calculates single point quant results for entire data frame
+    Parameters:
+        df (data frame): user excel sheet in pandas data frame
+        sample_name_list: list of all sample names in data frame
+        standards: dictionary of standards in formath {name: {"ID":, "Row", "Concentration"}}
+    Returns:
+        None - alters data frame in place, values now represent semi-quant values
+    """
+    for sample in sample_name_list:
+        for row in range(len(df)):
+
+            # get matching internal standards and native values needed for calculation
+            standard = find_matching_istd(df, row, standards)
+            standard_row = standards[standard]["Row"]
+            standard_concentration = standards[standard]["Concentration"]
+            native_height = df[sample][row]
+            istd_height = df[sample][standard_row]
+
+            # update data frame location with calculated value
+            df.loc[row, sample] = ((native_height / istd_height) *
+                                   float(standard_concentration)) / extraction_amount
+
+
+if __name__ == "__main__":
+
+    while True:
+
+        # get files and extraction amount from user
+        df_path = input("Enter full file path for data excel sheet: ")
+        df = pd.read_excel(df_path)
+
+        # set named constants and sample names from data frame
+        ISTD_MATCH_COLUMN = set_named_constant(df, r'number')
+        ANNOTATION_NAME_COLUMN = set_named_constant(df, r'name')
+        sample_names = set_sample_name_list(
+            df, r'[a-z0-9A-Z]+_[a-z0-9A-Z]+_[a-zA-Z]+')
+
+        # get standards csv file from user and populate standards dictionary
+        standards = set_standards_from_csv(df)
+
+        # get extraction amount from user for calculation
+        extraction_amount = float(
+            input("Enter how much sample was extracted (mL or mg): "))
+
+        # calculate results and save to new excel sheet
+        calculate_results(df, sample_names, standards)
+        path_obj = Path(df_path)
+        save_path = str(path_obj.parent/path_obj.stem) + \
+            '_SinglePointQuant.xlsx'
+        df.to_excel(save_path, index=False)
+
+        # allow user to repeat on another sheet, or exit program
+        again = input(
+            "Enter Y to repeat on another sheet or press enter to exit: ")
+        if again.lower() != "y":
+            break
